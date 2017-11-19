c74ee7e187f2e3a5e5f5324d3b4632910d921557
ckrgz001 <ckrgz001@outlook.com>
2017-11-19 15:23:01 -0500

    corrected

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -37,4 +37,22 @@ RSpec.describe AddressBook do
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
    end
+
+   describe "#remove_entry" do
+    it "removes the entry using the name, phone_number and email" do
+      book = AddressBook.new
+      book.add_entry('Random Entry', '010.012.2017', 'random.entry@specR.com')
+
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq('Random Entry')
+    end
+  end 
+
  end
