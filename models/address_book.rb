0bf1ed411a72499ff42f593707c1af06db214ede
ckrgz001 <ckrgz001@outlook.com>
2017-11-19 14:59:59 -0500

    Assignment attempt

--- models/address_book.rb
+++ models/address_book.rb
@@ -10,16 +10,29 @@ class AddressBook
 
 
 def add_entry(name, phone_number, email)
-    # #9
+   
     index = 0
     entries.each do |entry|
-    # #10
+   
       if name < entry.name
         break
       end
       index+= 1
     end
-    # #11
+
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+def remove_entry(name, phone_number, email)
+  
+    entries.each do |entry|
+        if name == entry.name
+            break
+        end
+        index = entries.indexOf(name)
+        entries.splice(index, 1)
+    end
+
+
+
 end
