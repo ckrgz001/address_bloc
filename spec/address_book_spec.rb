100fe9dbfe106b26f9ff4a2cdb638cb90bdb8084
ckrgz001 <ckrgz001@outlook.com>
2017-11-21 18:14:55 -0500

    Assignment ok

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -91,7 +91,35 @@ RSpec.describe AddressBook do
         check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
   end
-    
+  
+  describe "#import_from_entries_2.csv" do
+    it "imports the correct number of entries" do
+      
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry" do
+        book.import_from_csv("entries_2.csv")
+        entry_one = book.entries[0]
+  
+        check_entry(entry_one, "Cameron", "555-555-2222", "frye@bueller.com" )
+        
+    end
+
+    it "imports the 2nd entry" do
+        book.import_from_csv("entries_2.csv")
+        entry_two = book.entries[1]
+        check_entry(entry_two, "Ferris", "555-555-1111", "bueller@bueller.com")
+    end
+  
+    it "imports the 3rd entry" do
+        book.import_from_csv("entries_2.csv")
+        entry_three = book.entries[2]
+        check_entry(entry_three, "Sloane", "555-555-363360", "peterson@bueller.com")
+    end
+  end
   
 
    describe "#remove_entry" do
