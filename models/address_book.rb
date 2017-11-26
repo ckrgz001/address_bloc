b6898c7603e722b921355cf100f821684ed2b2b1
ckrgz001 <ckrgz001@outlook.com>
2017-11-26 18:34:53 -0500

    Assignment completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -48,13 +48,13 @@ def binary_search(name)
     lower = 0
     upper = entries.length - 1
 
-    # #2
+    
     while lower <= upper
-      # #3
+      
       mid = (lower + upper) / 2
       mid_name = entries[mid].name
 
-      # #4
+      
       if name == mid_name
         return entries[mid]
       elsif name < mid_name
@@ -63,11 +63,21 @@ def binary_search(name)
         lower = mid + 1
       end
     end
-
-    # #5
-    return nil
    
-end
+    return nil
+end 
 
+def iterative_search(name)
+    entries.each do |entry|
+        if name == entry.name
+            return entry 
+        end
+    end
+    return nil
 end
+end
+     
+
+
+
 
